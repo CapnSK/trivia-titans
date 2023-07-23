d5973ecfcf8c7ed0d3d0df68d365a10b7108ade5
Sankalp Kulkarni <sn573012@dal.ca>
2023-07-22 22:38:29 -0300

    Added api client and localstorage utility (example usage in login.js)

--- frontend/trivia_challenge_game/src/pages/Auth/Login/login.js
+++ frontend/trivia_challenge_game/src/pages/Auth/Login/login.js
@@ -1,12 +1,16 @@
 import { useContext, useEffect } from "react";
 import { AuthContext } from "../../../contexts";
 import { useNavigate } from "react-router-dom";
+import { axiosJSON } from "../../../lib/axios";
 
 function Login() {
 
     const { setAuthContext } = useContext(AuthContext);
     const navigate = useNavigate();
     console.info("Inside Login component");
+
+    //axiosJSON.post("https://run.mocky.io/v3/34cd8ec1-7f99-4cae-b35b-12e73d526b0d").then(console.log).catch(console.log);
+
     useEffect(() => {
         setTimeout(() => {
             console.info("Waiting for getting auth token data from backend");
